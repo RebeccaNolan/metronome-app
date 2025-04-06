df826f614652e3d4dec79358be8e59586f0e4c99
Rebecca Nolan <127899503+RebeccaNolan@users.noreply.github.com>
2025-04-06 01:23:33 +0100

    Imported Howler and created a method to play the click sound

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-homepage',
@@ -13,7 +14,7 @@ export class HomepageComponent {
 
   private startTime!: number;  // start time in milliseconds
   private timerInterval: any; // interval ID for the timer
-
+  private elapsedTime: number = 0; 
 
 // This function is called when the user clicks the button to start/stop the timer.
 // It checks if the timer is already running. If it is, it stops the timer and clears the interval.
@@ -21,28 +22,40 @@ export class HomepageComponent {
     if (this.isRunning) {
       this.isRunning = false;
       clearInterval(this.timerInterval);
+      this.elapsedTime += Date.now() - this.startTime;
     } else {
       this.isRunning = true;
       this.startTime = Date.now();
       this.timerInterval = setInterval(() => {
-        const elapsed = Date.now() - this.startTime;
-        this.timerDisplay = this.formatTime(elapsed);
+        const total = this.elapsedTime + (Date.now() - this.startTime); 
+        this.timerDisplay = this.formatTime(total);
       }, 10);  // update every 10ms
     }
   }
 
   // This function is called when the user clicks the button to reset the timer.
-  // It stops the timer, clears the interval, and resets the display to 00:00:00.
+  // It clears the interval, resets the elapsed time to 0, and updates the display to 00:00:00.
   resetTimer() {
-    this.isRunning = false;
     clearInterval(this.timerInterval);
-    this.timerDisplay = '00:00:00';
+    this.isRunning = false;
+    this.timerDisplay = '00:00:00'; 
+    this.startTime
+    this.elapsedTime = 0; 
   }
-  // This function formats the elapsed time in milliseconds into a string of the format mm:ss:SS.
+  //This function formats the elapsed time in milliseconds into a string of the format mm:ss:SS.
   private formatTime(ms: number): string {
     const minutes = Math.floor(ms / 60000).toString().padStart(2, '0');
     const seconds = Math.floor((ms % 60000) / 1000).toString().padStart(2, '0');
     const milliseconds = Math.floor((ms % 1000) / 10).toString().padStart(2, '0'); // show two digits
     return `${minutes}:${seconds}:${milliseconds}`;
+  }
+
+  clickSound = new Howl({
+    src: ['assets/click_woodblock.wav'],
+    volume: 1.0
+  });
+  
+  playClickSound() {
+    this.clickSound.play();
   }
 }
